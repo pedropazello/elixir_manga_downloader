fc5a02f51f221638d75be0750a3e99942d3d4b5a
Pedro Pazello <email@pedropazello.com>
2018-02-06 08:19:05 -0200

    Implement download of multiple chapters

--- lib/elixir_manga_downloader/downloader.ex
+++ lib/elixir_manga_downloader/downloader.ex
@@ -1,10 +1,24 @@
 defmodule ElixirMangaDownloader.Downloader do
-  def download do
-    "https://manga-fox.com/read-fullmetal-alchemist-manga/chapter-1"
-    |>get_html
-    |>find_pages_urls
-    |>download_pages
-    convert_pages(:pdf)
+  def async_download(last_chapter) do
+    Enum.each(1..last_chapter, fn i ->
+      spawn(ElixirMangaDownloader.Downloader, :download_chapter, [i])
+    end)
+  end
+
+  def download(last_chapter) do
+    Enum.each(1..last_chapter, fn i ->
+      download_chapter(i)
+    end)
+  end
+
+  def download_chapter(chapter) do
+    "https://manga-fox.com/read-fullmetal-alchemist-manga/chapter-#{chapter}"
+    |> get_html
+    |> find_pages_urls
+    |> download_pages(chapter)
+
+    convert_pages(chapter, :pdf)
+    IO.puts("Chapter #{chapter} done")
   end
 
   defp get_html(url) do
@@ -14,32 +28,37 @@ defmodule ElixirMangaDownloader.Downloader do
 
   defp find_pages_urls(html) do
     html
-    |>Floki.find(".content-area img")
-    |>Enum.map(fn(page) ->
-      {"img", img, _}      = page
+    |> Floki.find(".content-area img")
+    |> Enum.map(fn page ->
+      {"img", img, _} = page
       [_, _, {"src", src}] = img
       src
     end)
   end
 
-  defp download_pages(urls) do
+  defp download_pages(urls, chapter) do
     urls
-    |>Enum.with_index
-    |>Enum.each(fn({url, index}) ->
+    |> Enum.with_index()
+    |> Enum.each(fn {url, index} ->
       %{body: img} = HTTPoison.get!(url)
-      File.write!("/opt/elixir_manga_download/manga_#{index}.jpg", img)
+      File.mkdir("/opt/elixir_manga_download/#{chapter}")
+      File.write!("/opt/elixir_manga_download/#{chapter}/manga_#{index}.jpg", img)
     end)
   end
 
-  defp convert_pages(:pdf) do
-    options = ["/opt/elixir_manga_download/manga_*.jpg", "-quality",
-      "100", "/opt/elixir_manga_download/final_manga_01.pdf"]
+  defp convert_pages(chapter, :pdf) do
+    options = [
+      "/opt/elixir_manga_download/#{chapter}/manga_*.jpg",
+      "-quality",
+      "100",
+      "/opt/elixir_manga_download/final_manga_#{chapter}.pdf"
+    ]
+
     System.cmd("convert", options)
-    # System.cmd("rm", ["-f", "/opt/elixir_manga_download/manga_*"])
     :ok
   end
 
-  defp convert_pages(pages, :kindle) do
+  defp convert_pages(chapter, :kindle) do
     :ok
   end
 end
